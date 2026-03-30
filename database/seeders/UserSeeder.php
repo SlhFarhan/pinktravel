0c4bc5dbfc3059f16a485047b6f06313414a0a24
SlhFarhan <farhansolih0@gmail.com>
2026-03-30 18:16:42 +0700

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create Admin User
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@pinktravel.com',
+            'password' => Hash::make('password123'),
+            'role' => 'admin',
+        ]);
+
+        // Create Regular User
+        User::create([
+            'name' => 'John Doe',
+            'email' => 'user@pinktravel.com',
+            'password' => Hash::make('password123'),
+            'role' => 'user',
+        ]);
+    }
+}

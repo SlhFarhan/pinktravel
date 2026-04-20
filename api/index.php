4a81110bb7ea7067fd6abf98dd570653e65d3372
SlhFarhan <farhansolih0@gmail.com>
2026-04-20 10:32:48 +0700

    override SCRIPT_NAME to prevent Laravel stripping api prefix

--- api/index.php
+++ api/index.php
@@ -4,4 +4,7 @@
  * Forward Vercel requests to normal Laravel routing
  */
 
+// Override SCRIPT_NAME so Laravel doesn't mistake the /api folder as the base URL
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+
 require __DIR__ . '/../public/index.php';

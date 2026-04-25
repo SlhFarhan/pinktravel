0d07ac790bce2cce959ddcb5a9ffbb6bb6ead2c8
SlhFarhan <farhansolih0@gmail.com>
2026-04-25 20:01:20 +0700

    fix vercel

--- config/database.php
+++ config/database.php
@@ -85,7 +85,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE') . (str_contains(env('DB_HOST', ''), 'neon.tech') ? '?options=endpoint%3D' . explode('.', env('DB_HOST'))[0] : ''),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,

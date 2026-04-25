9349957cc108b5283ddb22b5298de0e09005694d
SlhFarhan <farhansolih0@gmail.com>
2026-04-25 18:51:46 +0700

    test webhook midtrans

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->validateCsrfTokens(except: [
             'midtrans-webhook',
+            '/midtrans-webhook',
         ]);
         $middleware->trustProxies(at: '*');
         $middleware->alias([

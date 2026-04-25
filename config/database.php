4aaa696e62e78681c4b537fbf8d0efa50203e600
SlhFarhan <farhansolih0@gmail.com>
2026-04-25 19:29:00 +0700

    fix

--- config/database.php
+++ config/database.php
@@ -95,7 +95,6 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => env('DB_SSLMODE', 'require'),
         ],
 
         'sqlsrv' => [

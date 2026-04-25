73d8f0b48c4f9844947a983f1ca89e47ee6fcd58
SlhFarhan <farhansolih0@gmail.com>
2026-04-25 20:13:52 +0700

    fix vercel

--- config/database.php
+++ config/database.php
@@ -89,7 +89,9 @@ return [
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => (str_contains(env('DB_HOST', ''), 'neon.tech') && env('DB_PASSWORD')) 
+                ? 'endpoint=' . explode('.', env('DB_HOST'))[0] . ';' . env('DB_PASSWORD') 
+                : env('DB_PASSWORD'),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
